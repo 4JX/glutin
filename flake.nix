6793f2fcce39a342c84c09c8fe0e58ef78b0701f
4JX <79868816+4JX@users.noreply.github.com>
2023-12-10 00:15:33 +0100

    Minimal repro

---
+++ flake.nix
@@ -0,0 +1,58 @@
+{
+  description = "Build env";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+
+    crane = {
+      url = "github:ipetkov/crane";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    flake-utils.url = "github:numtide/flake-utils";
+
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+        flake-utils.follows = "flake-utils";
+      };
+    };
+  };
+
+  outputs = { self, nixpkgs, crane, flake-utils, rust-overlay, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ (import rust-overlay) ];
+        };
+
+        rustVersion = "1.73.0";
+
+        rust = pkgs.rust-bin.stable.${rustVersion}.default.override {
+          extensions = [
+            "rust-src" # rust-analyzer
+          ];
+        };
+
+        nixLib = nixpkgs.lib;
+
+        runtimeDeps = with pkgs; [
+          # Yells not found
+          xorg.libX11
+          xorg.libXcursor
+          xorg.libXi
+
+          # Else "XKBNotFound" error
+          libxkbcommon
+        ] ++ sharedDeps;
+      in
+      {
+        devShells.rust = pkgs.mkShell {
+          LD_LIBRARY_PATH = nixLib.makeLibraryPath runtimeDeps;
+
+          buildInputs = [ rust ];
+        };
+      });
+}
